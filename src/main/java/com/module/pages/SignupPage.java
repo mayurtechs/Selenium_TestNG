c02e80e60deca5868ee7cc863a71d537ccfab112
mayurtechs <maddydiwan@gmail.com>
2022-08-15 06:02:09 -0700

    SignupPage has updated

--- src/main/java/com/module/pages/SignupPage.java
+++ src/main/java/com/module/pages/SignupPage.java
@@ -1,3 +1,29 @@
-By firstname = By.xpath("//input[@maxlength = '10']");
+package com.module.pages;
 
-By lastname  = By.xpath("//input[@maxlength = '15']");
+import org.openqa.selenium.By;
+
+import constants.Constant;
+
+public class SignupPage{
+
+
+ By firstname = By.xpath("//input[@maxlength = '10']");
+
+ By lastname  = By.xpath("//input[@maxlength = '15']");
+
+ By email = By.xpath("//input[@required = '']");
+ 
+ public void enterFirstName(String text)
+ {
+	 Constant.driver.findElement(firstname).sendKeys(text);
+ }
+ public void entrLastName(String text)
+ {
+	 Constant.driver.findElement(lastname).sendKeys(text);;
+ }
+ public void enterEmail(String text)
+ {
+	 Constant.driver.findElement(email).sendKeys(text);
+ }
+
+}
